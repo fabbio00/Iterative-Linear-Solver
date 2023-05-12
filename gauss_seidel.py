aa35784db6e931a20401b05a5fca7fb5f84d7afa
dadegrande99 <davidegrandesso@gmail.com>
2023-05-12 18:46:26 +0200

    testing gauss seidel

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -1,7 +1,38 @@
 import numpy as np
 
 
-def solve(self, mtxA, b, c):
-    vect_x = np.random.rand(mtxA.shape[1])
-    # print(mtxA.shape[1])
-    return vect_x
+def solve(self, mtxA, vectB, tol, maxIter):
+    # Creating utils variable
+    mtxP = mtxA.copy()
+    mtxN = mtxA.copy()
+    for i in range(len(mtxA)):
+        for j in range(len(mtxA)):
+            if (i <= j):
+                mtxN[i][j] = 0
+            else:
+                mtxN[i][j] = -mtxN[i][j]
+                mtxP[i][j] = 0
+
+    k = 0
+    vectX = np.random.rand(mtxA.shape[1])
+    residual = np.subtract(vectB, mtxA.dot(vectX))
+
+    while (np.linalg.norm(residual) > tol) and k < maxIter:
+        k += 1
+        for i in range(len(vectX)):
+            xi = vectB[i]
+            for j in range(len(vectX)):
+                xi -= mtxA[i][j] * vectX[j]
+            vectX[i] = xi/mtxA[i][i]
+        residual = np.subtract(vectB, mtxA.dot(vectX))
+        if k % 100 == 0:
+            print(k, end="\t")
+        if k % 1000 == 0:
+            print()
+            print(np.linalg.norm(residual))
+
+    if (np.linalg.norm(residual) > tol):
+        print("CIAO")
+    else:
+        print(tol)
+        print(np.linalg.norm(residual))
