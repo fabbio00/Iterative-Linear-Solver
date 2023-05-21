ce4f3e8a51905c8e2447c0cf435d798bdce17e0a
dadegrande99 <davidegrandesso@gmail.com>
2023-05-21 18:26:18 +0200

    class to library and correct return

--- gradiente_coniugato.py
+++ gradiente_coniugato.py
@@ -1,35 +1,44 @@
 import numpy as np
 from scipy.sparse.linalg import eigsh
-class GradC:
-    def __init__(self):
-        pass
-        
-    def execute(self, A, b, x, tol):
-        niter = 0
-        new_vector = np.asarray([0]*len(x))
-        residual = b - A.dot(new_vector)
-        dir = residual.copy()
-        while np.linalg.norm(residual)/np.linalg.norm(b) >= tol and niter <= 20000:
-            y = A.dot(dir)
-            z = A.dot(residual)
-            ak = (np.dot(dir, residual)) / (np.dot(dir, y))
-            new_vector = new_vector + (ak * dir)
-            residual = b - A.dot(new_vector)
-            w = A.dot(residual)
-            bk = (np.dot(dir, w)) / (np.dot(dir, y))
-            dir = residual - (bk*dir)
+from datetime import datetime
 
-            residual = b - A.dot(new_vector)
-            niter = niter + 1
-        return {"iter": niter, "err_rel": self.relative_error(x, new_vector)}
+def solve(A, b, x, tol):
+    start = datetime.now()
+    niter = 0
+    new_vector = np.asarray([0]*len(x))
+    residual = b - A.dot(new_vector)
+    dir = residual.copy()
+    while np.linalg.norm(residual)/np.linalg.norm(b) >= tol and niter <= 20000:
+        y = A.dot(dir)
+        z = A.dot(residual)
+        ak = (np.dot(dir, residual)) / (np.dot(dir, y))
+        new_vector = new_vector + (ak * dir)
+        residual = b - A.dot(new_vector)
+        w = A.dot(residual)
+        bk = (np.dot(dir, w)) / (np.dot(dir, y))
+        dir = residual - (bk*dir)
+
+        residual = b - A.dot(new_vector)
+        niter = niter + 1
     
-    def relative_error(self, x, xk1):
-        return np.linalg.norm(np.subtract(xk1, x))/np.linalg.norm(x)
-    
-    def convergenza(self, A):
-        conv = True
-        if(not np.allclose(A.toarray(), A.toarray().T)):
-            conv = False
-        if(not eigsh(A, k=1, which='LM')[0] > 0):
-            conv = False
-        return conv
+    end = datetime.now()
+    delta = end - start
+    # Risultato
+    res = {
+        "vectX": new_vector,
+        "nIter": niter,
+        "time": int(delta.total_seconds() * 1e6),
+        "eRel": relative_error(x, new_vector)
+    }
+    return res
+
+def relative_error(x, xk1):
+    return np.linalg.norm(np.subtract(xk1, x))/np.linalg.norm(x)
+
+def convergenza(A):
+    conv = True
+    if(not np.allclose(A.toarray(), A.toarray().T)):
+        conv = False
+    if(not eigsh(A, k=1, which='LM')[0] > 0):
+        conv = False
+    return conv
