e70ccbe7bcdabd43fc24b2189d2d082c3b1b3c70
dadegrande99 <davidegrandesso@gmail.com>
2023-06-03 11:02:17 +0200

    allineamneto nome variabili

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -5,35 +5,35 @@ from scipy.sparse.linalg import spsolve_triangular
 from datetime import datetime
 
 
-def solve(mtxA, vectB, vectX, tol):
-    if(cr.row_diagonal_dominance(mtxA)):
+def solve(A, b, x, tol):
+    if (cr.row_diagonal_dominance(A)):
         print("La matrice A è a dominanza diagonale per righe, quindi converge")
     else:
         print("La matrice A non è a dominanza diagonale per righe, quindi non è detto che converga")
     start = datetime.now()
     # Variabili
     maxIter = 20000
-    mtxP = tril(mtxA, format="csr")
+    mtxP = tril(A, format="csr")
     k = 0
-    vectX1 = np.zeros(mtxA.shape[0])
-    residual = vectB - mtxA.dot(vectX1)
+    vectX1 = np.zeros(A.shape[0])
+    residual = b - A.dot(vectX1)
 
     # Funzione
-    while np.linalg.norm(residual)/np.linalg.norm(vectB) >= tol and k <= maxIter:
+    while np.linalg.norm(residual)/np.linalg.norm(b) >= tol and k <= maxIter:
         k += 1
         vectX1 = vectX1 + spsolve_triangular(mtxP, residual, lower=True)
-        residual = vectB - mtxA.dot(vectX1)
+        residual = b - A.dot(vectX1)
 
     end = datetime.now()
     delta = end - start
     if k > 20000:
-        if np.linalg.norm(residual)/np.linalg.norm(vectB) > tol:
+        if np.linalg.norm(residual)/np.linalg.norm(b) > tol:
             print("superato il numero massimo di iterazioni")
     # Risultato
     res = {
-        "vectX": vectX1,
+        "x": vectX1,
         "nIter": k,
         "time": int(delta.total_seconds() * 1e6),
-        "eRel": cr.relative_error(vectX, vectX1)
+        "eRel": cr.relative_error(x, vectX1)
     }
     return res
--- gradiente.py
+++ gradiente.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import control as cr
 
 
-def solve(mtxA, vectB, vectX, tol):
-    if (cr.is_sim_pos(mtxA)):
+def solve(A, b, x, tol):
+    if (cr.is_sim_pos(A)):
         print("La matrice A è simmetrica e definita positiva, quindi converge per ogni valore del vettore iniziale")
     else:
         print("La matrice A non è simmetrica o definita positiva")
@@ -12,31 +12,31 @@ def solve(mtxA, vectB, vectX, tol):
 
     # Variabili
     k = 0
-    vectX1 = np.zeros(mtxA.shape[0])
-    residual = vectB - mtxA.dot(vectX1)
+    vectX1 = np.zeros(A.shape[0])
+    residual = b - A.dot(vectX1)
 
     # Funzione
-    while np.linalg.norm(residual)/np.linalg.norm(vectB) >= tol and k <= 20000:
+    while np.linalg.norm(residual)/np.linalg.norm(b) >= tol and k <= 20000:
         k += 1
-        y = mtxA.dot(residual)
+        y = A.dot(residual)
         a = residual.T.dot(residual)
         b = residual.T.dot(y)
         alpha = a/b
 
         vectX1 = vectX1 + alpha * residual
 
-        residual = vectB - mtxA.dot(vectX1)
+        residual = b - A.dot(vectX1)
 
     end = datetime.now()
     delta = end - start
     if k > 20000:
-        if np.linalg.norm(residual)/np.linalg.norm(vectB) >= tol:
+        if np.linalg.norm(residual)/np.linalg.norm(b) >= tol:
             print("superato il numero massimo di iterazioni")
     # Risultato
     res = {
-        "vectX": vectX1,
+        "x": vectX1,
         "nIter": k,
         "time": int(delta.total_seconds() * 1e6),
-        "eRel": cr.relative_error(vectX, vectX1)
+        "eRel": cr.relative_error(x, vectX1)
     }
     return res
